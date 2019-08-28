6342943a8b6d8c6443855b4957f649be9c7b2d09
omar elgendy <omar.elgendy@re-lab.it>
2019-08-28 09:21:43 +0200

    removed test directory --unneeded

--- test/speech.py
+++
@@ -1,9 +0,0 @@
-import speech_recognition as sr
-
-# print(sr.__version__)
-
-r = sr.Recognizer()
-
-audio_source = sr.AudioData('harvard.wav', 16000, 2)
-
-r.recognize_google(audio_source)
--- test/test_x.py
+++
@@ -1,8 +0,0 @@
-import xml.etree.ElementTree as et
-
-
-tree = et.parse('test.xml')
-root = tree.getroot()
-
-for child in root:
-	print(child.tag, child.attrib)
